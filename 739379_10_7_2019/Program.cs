06818ccb4ca2dbe0a402b485e35f55ff1490068a
Sandeep <ssd2192@gmail.com>
2019-07-10 09:09:46 -0400

    A-A

---
+++ 739379_10_7_2019/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _739379_10_7_2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tp = new TextProcessing();
+            tp.Run();
+        }
+    }
+
+    class TextProcessing
+    {
+        public void Run()
+        {
+            // Open the names file and access the data
+            using (StreamReader file = new StreamReader("D:/CSHARP/739379_10_7_2019/739379_10_7_2019/Names.txt"))
+            {
+                int counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    counter++;
+                }
+                file.Close();
+                Console.WriteLine($"File has {counter} lines.");
+            }
+        }
+    }
+}
