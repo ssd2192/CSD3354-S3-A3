5da7a9569e0bc945ed24964bf9f78745b8f353e7
Sandeep <ssd2192@gmail.com>
2019-07-10 11:42:21 -0400

    A-C

--- 739379_10_7_2019/Program.cs
+++ 739379_10_7_2019/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,15 +14,18 @@ namespace _739379_10_7_2019
         {
             var tp = new TextProcessing();
             tp.Run();
+            Console.ReadKey();
         }
     }
 
     class TextProcessing
     {
         Queue<string> names = new Queue<string>();
+
+        //Stack<string> namesStack = new Stack<string>();
         public void Run()
         {
-            
+
             // Open the names file and access the data
 
             using (StreamReader file = new StreamReader("D:/CSHARP/739379_10_7_2019/739379_10_7_2019/Names.txt"))
@@ -35,8 +39,50 @@ namespace _739379_10_7_2019
                     names.Enqueue(ln);
                     counter++;
                 }
+                //Printing names for queue
+
+                foreach (Object obj in names)
+                {
+                    Console.WriteLine(obj);
+
+                }
+
+                //closing file
+               
                 file.Close();
                 Console.WriteLine($"File has {counter} lines.");
+
+                PlayingWithStacks.Run(names);
+
+
+            }
+
+        }
+
+        class PlayingWithStacks
+        {
+            public static void Run(Queue<string> tangerine)
+            {
+                Stack<string> nameStack = new Stack<string>();
+                // how can I access the Queue variables "names" here in this class???
+                // iterate OVER our Queue 
+                // get each element
+                // push it into the Stack
+                //foreach (Object item in tangerine)
+                //{
+                //    while (tangerine.Count() > 0)
+                //        nameStack.Push(tangerine.Dequeue());
+                //}
+
+                for(int i=0; i < tangerine.Count(); i++)
+                {
+                    nameStack.Push(tangerine.Dequeue());
+                    i--;
+                }
+                foreach (var itm in nameStack)
+                    Console.WriteLine(itm);
+
+
             }
         }
     }
