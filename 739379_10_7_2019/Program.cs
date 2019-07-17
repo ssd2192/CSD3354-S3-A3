595ca27d92e671062145fa7051857b890244e2bf
Sandeep <ssd2192@gmail.com>
2019-07-17 09:29:46 -0400

    A-F

--- 739379_10_7_2019/Program.cs
+++ 739379_10_7_2019/Program.cs
@@ -8,6 +8,8 @@ using System.Threading.Tasks;
 
 namespace _739379_10_7_2019
 {
+    
+    
     class Program
     {
         static void Main(string[] args)
@@ -22,8 +24,7 @@ namespace _739379_10_7_2019
     {
         Queue<string> names = new Queue<string>();
 
-        static Stack<string> nameStack = new Stack<string>();
-        static LinkedList<string> namesList = new LinkedList<string>();
+        
         public void Run()
         {
 
@@ -53,64 +54,84 @@ namespace _739379_10_7_2019
                 Console.WriteLine($"File has {counter} lines.");
 
                 // Calling Functions Using Class
-                PlayingWithStacks.Run(names);
-                PlayingWithLinkedList.Run(nameStack);
+                Stack<string> nameStack = PlayingWithStacks.Run(names);
+                LinkedList<string> namesList = PlayingWithLinkedList.Run(nameStack);
                 PlayingWithLinkedList.Sort(namesList);
-                
-            }
-
-        }
-
-        class PlayingWithStacks
-        {
-            public static void Run(Queue<string> tangerine)
-            {
-                //Stack<string> nameStack = new Stack<string>();
-                // how can I access the Queue variables "names" here in this class???
-                // iterate OVER our Queue 
-                // get each element
-                // push it into the Stack
-                //foreach (Object item in tangerine)
-                //{
-                //    while (tangerine.Count() > 0)
-                //        nameStack.Push(tangerine.Dequeue());
-                //}
-
-                for(int i=0; i < tangerine.Count(); i++)
-                {
-                    nameStack.Push(tangerine.Dequeue());
-                    i--;
-                }
-                foreach (var itm in nameStack)
-                    Console.Write(itm +", ");
 
             }
-        }
-        class PlayingWithLinkedList
-        {
-            public static void Run(Stack<string> tangerine)
-            {
-                //LinkedList<string> namesList = new LinkedList<string>();
-
-                while (tangerine.Count()>0)
-                {
-                    namesList.AddFirst(tangerine.Pop());
-
-                }
-                Console.WriteLine("______________________________");
-                foreach (var itm in namesList)
-                    Console.Write(itm + ", ");
-               
-            }
-            // Sort Method For Linked List
-            public static void Sort(LinkedList<string> listsort)
-            {
-                Console.WriteLine("______________________________");
-                foreach (var itm in listsort.OrderBy(i => i))
-                    Console.Write(itm + ", ");
-            }
-
 
         }
     }
+    class PlayingWithStacks
+    {
+        static Stack<string> nameStack = new Stack<string>();
+        public static Stack<string> Run(Queue<string> tangerine)
+        {
+            //Stack<string> nameStack = new Stack<string>();
+            // how can I access the Queue variables "names" here in this class???
+            // iterate OVER our Queue 
+            // get each element
+            // push it into the Stack
+            //foreach (Object item in tangerine)
+            //{
+            //    while (tangerine.Count() > 0)
+            //        nameStack.Push(tangerine.Dequeue());
+            //}
+
+            for (int i = 0; i < tangerine.Count(); i++)
+            {
+                nameStack.Push(tangerine.Dequeue());
+                i--;
+            }
+            foreach (var itm in nameStack)
+                Console.Write(itm + ", ");
+
+            return nameStack;
+
+        }
+    }
+    class PlayingWithLinkedList
+    {
+        static LinkedList<string> namesList = new LinkedList<string>();
+        public static LinkedList<string> Run(Stack<string> tangerine)
+        {
+            //LinkedList<string> namesList = new LinkedList<string>();
+
+            while (tangerine.Count() > 0)
+            {
+                namesList.AddFirst(tangerine.Pop());
+
+            }
+            Console.WriteLine("______________________________");
+            foreach (var itm in namesList)
+                Console.Write(itm + ", ");
+
+            return namesList;
+
+        }
+        // Sort Method For Linked List
+        public static void Sort(LinkedList<string> listsort)
+        {
+            Console.WriteLine("______________________________");
+            foreach (var itm in listsort.OrderBy(i => i))
+                Console.Write(itm + ", ");
+        }
+    }
+    //class Node
+    //{
+    //    public Node nextLink;
+    //    public Node prevLink;
+    //    public string nodeName;
+    //}
+
+    //class LinkedList
+    //{
+    //    Node Head, Orange, Tangerine, Tail;
+    //    public void CreateLinkedList()
+    //    {
+    //        Head = new Node();
+    //        Head.nextLink = Orange();
+    //    }
+    //}
+
 }
